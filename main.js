84059d81340aa017fa638517cc4f59c7a3d0ea65
Xingan Wang <wangxgwxg@gmail.com>
2017-05-10 22:49:38 -0700

    add some code

--- main.js
+++ main.js
@@ -3,11 +3,14 @@ const reconcilliation = require('./lib/reconciliation')
 const jsdom = require("jsdom")
 const { JSDOM } = jsdom
 
-fs.readFile('test_files/bootstrap.html', (err, data) => {
+fs.readFile('html_files/bootstrap.html', (err, data) => {
   if (err) {
     throw err
   }
   const sourceString = data.toString()
   const source = new JSDOM(sourceString).window.document
-  // ...
+  const targetString = sourceString.slice(0)
+  const target = new JSDOM(targetString).window.document
+
+  reconcilliation.diff(source, target)
 })
