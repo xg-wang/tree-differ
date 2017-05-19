22f3c11820858e41e2d9fd3b3db7a28f5a48be97
Shihao Xu <xsh6528@gmail.com>
2017-05-19 16:19:11 -0700

    change input

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 const fs = require('fs')
 const reconcilliation = require('./lib/reconciliation')
 const jsdom = require("jsdom")
-const { JSDOM } = jsdom
+const {JSDOM} = jsdom
 
 fs.readFile('html_files/bootstrap.html', (err, data) => {
-  if (err) {
-    throw err
-  }
-  const sourceString = data.toString()
-  const source = new JSDOM(sourceString).window.document
-  const targetString = sourceString.slice(0)
-  const target = new JSDOM(targetString).window.document
+    if (err) {
+        throw err
+    }
+    const sourceString = data.toString()
+    const source = new JSDOM(sourceString).window.document.body
+    const targetString = sourceString.slice(0)
+    const target = new JSDOM(targetString).window.document.body
 
-  reconcilliation.diff(source, target)
+    reconcilliation.diff(source, target)
 })
