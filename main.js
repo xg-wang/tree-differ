5f60062c3c31da2b3115a507a03203a1803e449c
Xingan Wang <wangxgwxg@gmail.com>
2017-06-06 17:14:27 -0700

    change return type and way to call apply

--- main.js
+++ main.js
@@ -20,14 +20,14 @@ Promise.all([
     readFileString('html_files/list1.html'),
     readFileString('html_files/list2.html')
 ]).then(values => {
-    const base = new JSDOM(values[0]).window.document.body
+    let base = new JSDOM(values[0]).window.document.body
     const target = new JSDOM(values[1]).window.document.body
 
     var changes
 
     changes = reconcilliation.diff(target, base)
     console.dir(changes, {depth: 3, colors: true})
-    reconcilliation.apply(changes)
+    base = reconcilliation.apply(changes)
     if (base.isEqualNode(target)) {
         console.log("Diff & Apply Success ~")
     } else {
