ec47d0d036ef51d71b84d86df9268b762ee3941a
Xingan Wang <wangxgwxg@gmail.com>
2017-06-09 15:08:18 -0700

    preprocess

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
 const assert = require('assert')
 const fs = require('fs')
 const reconciliation = require('./lib/reconciliation')
+const zhsh = require('./lib/zhsh')
 const {JSDOM} = require("jsdom")
 const {readFileString} = require('./lib/utils')
 
 // TODO add test cases: removeChildElement, insertChildElement, moveChildElement
 
 Promise.all([
-    readFileString('html_files/list1.html'),
-    readFileString('html_files/list2.html')
+    readFileString('html_files/newTestCase1.html'),
+    readFileString('html_files/newTestCase2.html')
 ]).then(values => {
     let base = new JSDOM(values[0]).window.document.body
     const target = new JSDOM(values[1]).window.document.body
     const targetClone = target.cloneNode(true)
 
-    const changes = reconciliation.diff(target, base)
+    const changes = zhsh.diff(target, base)
     assert(target.isEqualNode(targetClone), 'target is same')
     // console.dir(changes, {depth: 3, colors: true})
 
