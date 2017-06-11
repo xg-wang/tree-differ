04b3e211370ac101c8ba36447aa4b0b2d80ecfe4
Xingan Wang <wangxgwxg@gmail.com>
2017-06-11 10:16:35 -0700

    timeout and sample result

--- test/performance.js
+++ test/performance.js
@@ -24,7 +24,7 @@ describe('performance tests', function() {
           changes = reconciliation.diff(target, base)
         })
 
-        it('total time', function() {
+        it('apply time', function() {
           base = reconciliation.apply(base, changes)
         })
 
@@ -32,7 +32,30 @@ describe('performance tests', function() {
           assert(base.isEqualNode(target))
         })
       })
-
     })
   }
+
+  describe('Express_trimmed, Zhang Shasha', function() {
+    let base = null, target = null, changes = null
+    before(function() {
+      const baseString = fs.readFileSync(`${dirRoot}/express_trimmed/base.html`, {encoding: 'utf8'})
+      const targetString = fs.readFileSync(`${dirRoot}/express_trimmed/target.html`, {encoding: 'utf8'})
+      base = new JSDOM(baseString).window.document.body
+      target = new JSDOM(targetString).window.document.body
+    })
+
+    it('diff time', function() {
+      this.timeout(100000)
+      changes = zhsh.diff(target, base)
+    })
+
+    it('apply time', function() {
+      base = zhsh.apply(base, changes)
+    })
+
+    after('base should equal target after apply', function() {
+      assert(base.isEqualNode(target))
+    })
+  })
+
 });
