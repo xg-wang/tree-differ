5ec03b2a6d61203c8b77f41e001e4e84319e5002
Xingan Wang <wangxgwxg@gmail.com>
2017-06-11 09:37:49 -0700

    fix correctness test

--- test/correctness.js
+++ test/correctness.js
@@ -11,11 +11,12 @@ describe('correctness tests', function() {
 
   for (let dir of dirs) {
     describe(`${dir}`, function() {
-      let base = null, target = null
+      let base = null, base2 = null, target = null
       beforeEach(function() {
         const baseString = fs.readFileSync(`${dirRoot}/${dir}/base.html`, {encoding: 'utf8'})
         const targetString = fs.readFileSync(`${dirRoot}/${dir}/target.html`, {encoding: 'utf8'})
         base = new JSDOM(baseString).window.document.body
+        base2 = base.cloneNode(true)
         target = new JSDOM(targetString).window.document.body
       })
 
@@ -25,9 +26,9 @@ describe('correctness tests', function() {
         assert(base.isEqualNode(target))
       })
       it('zhang shasha should pass', function() {
-        const changes = zhsh.diff(target, base)
-        base = zhsh.apply(base, changes)
-        assert(base.isEqualNode(target))
+        const changes = zhsh.diff(target, base2)
+        base2 = zhsh.apply(base2, changes)
+        assert(base2.isEqualNode(target))
       })
     })
   }
